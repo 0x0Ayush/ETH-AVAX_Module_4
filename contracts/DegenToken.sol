229fe4829ead0eeb3d6f0069cb7bb63cdf7b540c
MetaPunks <99666258+punks783@users.noreply.github.com>
2023-07-30 18:42:51 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
 contract DegenToken {
-    string public name = "Degen";
-    string public symbol = "DGN";
+    string public name = "Degen Gaming Token";
+    string public symbol = "DG";
     uint8 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10**uint256(decimals)); // Initial supply: 1,000,000 DGN
+    uint256 public totalSupply;
 
     address public owner;
     mapping(address => uint256) public balanceOf;
@@ -26,8 +26,10 @@ contract DegenToken {
         _;
     }
 
-    constructor() {
+    constructor(uint256 initialSupply) {
         owner = msg.sender;
+        totalSupply = initialSupply * (10**uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
 
         // Initialize the shop with item prices and stocks
         itemPrices[1] = 100; // Item ID 1: Food
@@ -39,9 +41,6 @@ contract DegenToken {
         itemStocks[2] = 800;  // Initial stock of Water
         itemStocks[3] = 500;  // Initial stock of Power
         itemStocks[4] = 300;  // Initial stock of Health
-
-        // Allocate the total supply to the contract owner
-        balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address to, uint256 value) external returns (bool) {
@@ -99,6 +98,7 @@ contract DegenToken {
 
         emit Burn(msg.sender, value);
     }
+    
     function buyItem(uint256 itemId, uint256 quantity) external {
         require(itemId >= 1 && itemId <= 4, "Invalid item ID");
         require(itemPrices[itemId] > 0, "Item not available");
